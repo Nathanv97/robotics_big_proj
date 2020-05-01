06a6a195a9d2afc64b6c320c3f296315c8994b13
nathan vanderheiden <nvanderheiden@ou.edu>
2020-05-01 11:46:20 -0500

    code clean up

--- big_project_pkg/src/landmarkID.py
+++ big_project_pkg/src/landmarkID.py
@@ -14,6 +14,7 @@ def callback(msg):
     	x = msg.pose.pose.position.x
     	y = msg.pose.pose.position.y
 	
+	# calculate the distance from the robot to all the landmarks
 	CS_lab     = distance(x,y,1.2,-18.3)
 	REPF       = distance(x,y,-15.9,-16.1)
 	dev_atrium = distance(x,y,0.9,-4.4)
@@ -21,6 +22,7 @@ def callback(msg):
 	stairs     = distance(x,y,1.7,11.0)
 	admin_off  = distance(x,y,11.7,9.7)
 
+	# If the robot is close enough to any of the landmarks then print it out to loginfo
 	if CS_lab < 1:
 		rospy.loginfo('This is the CS Laboratory')
 	elif REPF < 5:
